f67efd73b5d34419d9c14095db9c031c58e31682
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-07-01 16:32:40 -0400

    commit tree 89a8d9fc42b150967abb35696439d4dd36ddff99

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,32 @@
 # your code here
+class MySet
+    
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+    
+
+    # set = MySet.new([1,2,3,3])
+end
